9a5380c3449bd51f2ec1f3c44d6c5993178d4b1b
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-10-12 22:04:32 -0300

    adding files with all functions

--- script.js
+++ script.js
@@ -1,148 +1,104 @@
-function Book(title, author, pages, read){
-      
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read  
-   
+//button event listeners for create new book, add new book to page, close popup
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
 
+const newBookBtn = document.querySelector('#newBtn');
+newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+
+const popUpForm = document.getElementById('popUp');
+const closePopUp = document.getElementsByTagName('span')[0];
+closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
+
+//Book Constructor
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = form.title.value; 
+        this.author = form.author.value; 
+        this.pages = form.pages.value + ' pg'; 
+        this.read = form.read.checked; 
+    }
 }
 
+//creates book from Book Constructor, adds to library
 
+let myLibrary = [];
+let newBook;
 
 function addBookToLibrary(){
-    this.books =[];
-    this.addBooks = function(books){
-        // deletes the last properties added to the array books.
-        this.books.pop();
-        // adds the newest properties added from the user
-        this.books.push(books)
-    }
-   
+    event.preventDefault();
+    popUpForm.style.display= "none";
+
+    newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+    form.reset();
 }
- // new book object
- let book = new addBookToLibrary();
 
-// adding book's info to html>div
+//creates a visual book
+function render(){
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
 
-function addToHtml(){
+    for (let i=0; i<myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
+}
 
-    //  
-     
-// remove button
+// creates book DOM elements for rendering
 
-
-    // where it will be added to > the container
-    let containerDiv = document.querySelector(".container");
-    // creating div inside the container:
-    const content = document.createElement('div');
-    content.classList.add('content');
+function createBook(item) {
+    const library = document.querySelector('#Library-container');
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
     
-    for(let i = 0; i <book.books.length; i++){
-        containerDiv.appendChild(content);
-        // creating the item in the content div
-        let title = document.createElement('p');
-        let author =document.createElement('p');
-        let pages =document.createElement('p');
-        let read =document.createElement('button');
-        // adding an <hr>
-        let hr = document.createElement('hr')
-        hr.classList.add("hr");
-        //adding delete button
-         let deleteBtn = document.createElement("button");
-        deleteBtn.textContent= "Delete";
-        deleteBtn.classList.add("bookDelete")
-       
-               
-        // taking the values from the array
-        let book_title = book.books[i].title;
-        let book_author = book.books[i].author;
-        let book_pages = book.books[i].pages;
-        let book_read = book.books[i].read;
-     
-      
+    
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
 
-        // adding text to the items just created
-        title.textContent=book_title;
-        author.textContent=book_author;
-        pages.textContent=book_pages;
-        read.textContent=book_read;
-                
-        //appending the children to .content
-        content.appendChild(title);
-        content.appendChild(author);
-        content.appendChild(pages);
-        content.appendChild(read);
-        content.appendChild(deleteBtn);
-        content.appendChild(hr);
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
 
-       // /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
-// https://stackoverflow.com/questions/59875808/deleting-newly-added-elements-by-adding-event-listeners-to-them
-const ul = document.querySelector('.hr');
+    authDiv.textContent = item.author;
+    authDiv.classList.add('author');
+    bookDiv.appendChild(authDiv);
 
-//Creating delete button
-function createDeleteButton() {
-  const delButton = document.createElement("button");
-  delButton.classList.add("bookDelete");
-  delButton.type = 'button';
-  delButton.appendChild(document.createTextNode("Delete"));
-  return delButton;
-}
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
 
-// event listener that removes list element when button is clicked
-function deleteListElement(event) {
-  const {
-    target: btn
-  } = event; // get the target property from the event object as a local variable btn
-  if (btn.matches('button.bookDelete')) {
-    btn.closest('hr').removeChild(btn.parentElement);
-  }
-}
-
-ul.addEventListener('click', deleteListElement);
-
-document.getElementById('addListElement').addEventListener('click', function() {
-  const para = document.createElement('p');
-  para.classList.add('pDelete')
-  const btn = createDeleteButton();
-  para.appendChild(btn);
-  ul.appendChild(para);
-})
-
-// /*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*
-        // adding classes to it
-        title.classList.add('bookTitle');
-        author.classList.add("bookAuthor");
-        pages.classList.add("bookPages");
-
-        // conditionals > changes the class accourding the user's input: read/not read
-        if(book.books[i].read === 'read'){
-            read.classList.add("bookRead");
-        }
-        else{
-            read.classList.add("bookUnRead");
-        }
-                  
+    readBtn.classList.add('readBtn')    
+    bookDiv.appendChild(readBtn);
+    if(item.read===false){
+        readBtn.textContent = 'Not read';
+        readBtn.style.backgroundColor = 'hsl(0deg 100% 81%)';
+    }else{
+        readBtn.textContent = 'Read';
+        readBtn.style.backgroundColor = 'hsl(118deg 100% 81%)' ;
     }
-}
+		
 
-//adding book button
-let addingBookButton = document.getElementById('addBtn');
-addingBookButton.addEventListener('click' , function getTarget(e){
-    e.preventDefault()
-    // getting input from user
-    let jsTitleInput = document.getElementById('titleInput').value;
-    let jsAuthorInput = document.getElementById('authorInput').value;
-    let jsPagesInput = document.getElementById('pagesInput').value + " pages";
+    removeBtn.textContent = 'Remove'; 
+    removeBtn.setAttribute('id', 'removeBtn');
+    bookDiv.appendChild(removeBtn);
+    
+    library.appendChild(bookDiv);
 
-    // getting input from radio buttons from user> read || not_read
-    let form  = document.getElementById('form')
-    let jsReadInput = form.elements["read"].value;
-       
-    let newBook = new Book(jsTitleInput, jsAuthorInput,jsPagesInput, jsReadInput);
-  
-    book.addBooks(newBook);
-    addToHtml();
-   
-})
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(item),1);
+      //  setData()
+        render();
+    });
 
+    //add toggle ability to each book 'read' button on click
+    readBtn.addEventListener('click', () => { 
+        item.read = !item.read; 
+   //     setData(); 
+        render();
+    }); 
+};
